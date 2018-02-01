163d536fa25af921e7941983d141f7243ba5063d
Alexandre Dias <alexandre.dias@marvil56.com>
2018-02-01 14:12:58 +0000

    fix some errors php install

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# install_LAMP
+# Install LAcMP
 Install Linux Apache connection to Mariadb and PHP
+
+[project] = Project Name
+[environment] = Development {dev} / Staging {stag} / Production {prod}
+[domain] = example.com
+
+NAME=[project] ID=[environment] DOMAIN=[domain] ./install.sh
--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 echo $NAME
 echo $ID
 echo $DOMAIN
-sudo apt install update -y
-sudo apt install upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
 sudo apt auto-remove -y
 
 echo "\o/"
@@ -13,13 +13,12 @@ sudo service apache2 start
 
 echo "\o/ \o/ \o/"
 
-sudo apt install libapache2-mod-php libapache2-mod-php7.0 php-common php7.0 php7.0-cli php7.0-common php7.0-json php7.0-opcache
-  php7.0-readline
-sudo apt install git php-mysql
-sudo apt install update -y
-sudo apt install upgrade -y
-sudo apt install mariadb-client-core-10.0
-sudo apt install php7.0-curl php7.0-gd php7.0-intl php-pear php7.0-imap php7.0-mcrypt php7.0-ps php7.0-pspell php7.0-recode php7.0-snmp php7.0-tidy php7.0-xmlrpc php7.0-xsl
+sudo apt install libapache2-mod-php libapache2-mod-php7.0 php-common php7.0 php7.0-cli php7.0-common php7.0-json php7.0-opcache php7.0-readline -y
+sudo apt install git php-mysql -y
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install php7.0-curl php7.0-gd php7.0-intl php-pear php7.0-imap php7.0-mcrypt php7.0-ps php7.0-pspell php7.0-recode php7.0-snmp php7.0-tidy php7.0-xmlrpc php7.0-xsl -y
+sudo apt install mariadb-client-core-10.0 -y
 
 echo "\o/ \o/ \o/ \o/"
 
@@ -27,8 +26,9 @@ sudo chown ubuntu:ubuntu -R /var/www/
 mkdir /var/www/$ID-$NAME
 
 echo " \o/ \o/ \o/ \o/ \o/"
-awk '{gsub("folder", "$ID-$NAME")}1' example.conf > example.tmp && mv example.tmp example_1.conf
-awk '{gsub("domain", "$DOMAIN")}1' example_1.conf > example_1.tmp && mv example_1.tmp $ID-$NAME.conf
+awk '{gsub("folder", $ID-$NAME)}1' example.conf > example.tmp && mv example.tmp example_1.conf
+awk '{gsub("domain", $DOMAIN)}1' example_1.conf > example_1.tmp && mv example_1.tmp $ID-$NAME.conf
+sudo mv ~/install_LAMP/$ID-$NAME.conf /etc/apache2/sites-avaiable/$ID-$NAME.conf
 sudo a2dissite 000-default.conf
 sudo a2ensite $ID-$NAME.conf
 
